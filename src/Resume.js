4d512964262d9f8d352b920e900c225cdad86ac1
TessaNyquist <tessanyquist@outlook.com>
2023-08-18 16:01:50 -0400

    Resume Link partially working

--- src/Resume.js
+++ src/Resume.js
@@ -1,10 +1,26 @@
 import React from "react";
+import ResumePdf from "./Images/Software-Developer-Résumé.pdf";
+import "./Resume.css"; // Import the corresponding CSS file
 
 function Resume() {
+  const downloadResume = () => {
+    const element = document.createElement("a");
+    element.href = ResumePdf;
+    element.download = "Software-Developer-Resume.pdf";
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  };
   return (
-    <div>
-      <h1>My Resume</h1>
-      <p>This is my resume content.</p>
+    <div classname="resume">
+      <h1>My Résumé</h1>
+      <div classname="resume-pdf">
+        <iframe src={ResumePdf} title="My Resume"></iframe>
+      </div>
+      {/* Display the PDF using an iframe */}
+      <button className="download-button" onClick={downloadResume}>
+        Download My Résumé
+      </button>
     </div>
   );
 }
