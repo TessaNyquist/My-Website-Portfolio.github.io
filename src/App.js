5b1fb5b057e8c06cc02053b26d8c8297b24c497d
TessaNyquist <tessanyquist@outlook.com>
2023-08-18 15:11:29 -0400

    idk

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from "./Components/Navbar";
 import AboutMe from "./Components/AboutMe";
+<<<<<<< Updated upstream
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Projects from "./Projects";
 import Resume from "./Resume";
 import Blog from "./Blog";
+=======
+import Footer from "./Components/Footer";
+//import Home from './Home';
+import Projects from './Projects';
+import Resume from './Resume';
+import Blog from './Blog';
+>>>>>>> Stashed changes
 import { Data } from "./Components/Data";
 import { ToolsData } from "./Components/ToolsData";
 import LanguageGrid from "./Components/LanguageGrid";
-import Footer from "./Components/Footer";
-//import ContactMeForm from "./Components/ContactMe";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
+    <div className="App">
+      <Router>
         <Navbar />
+<<<<<<< Updated upstream
         <AboutMe />
 
         <Routes>
@@ -37,6 +46,31 @@ function App() {
         </footer>
       </div>
     </Router>
+=======
+        <Routes>
+          <Route exact path="/">
+            <AboutMe />
+            <header className="App-header">
+              <p className="skillsTitle"> Languages/Frameworks</p>
+              <LanguageGrid className="LanguageGrid" data={Data} />
+              <p className="skillsTitle"> Tools</p>
+              <LanguageGrid className="LanguageGrid" data={ToolsData} />
+            </header>
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/resume">
+            <Resume />
+          </Route>
+          <Route path="/blog">
+            <Blog />
+          </Route>
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+>>>>>>> Stashed changes
   );
 }
 
