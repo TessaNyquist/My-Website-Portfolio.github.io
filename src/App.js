4e5afa5edd980df03643ed0923834484d53cd140
TessaNyquist <tessanyquist@outlook.com>
2023-08-19 16:00:16 -0400

    Fix routing probelm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Navbar from "./Components/Navbar";
-//import AboutMe from "./Components/AboutMe";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
@@ -17,10 +16,11 @@ function App() {
       <div className="App">
         <Navbar />
         <Routes>
-          <Route path="/Home" element={<Home />}/>
-          <Route path="/Projects" element={<Projects />} />
-          <Route path="/resume" element={<Resume />}/>
-          <Route path="/blog" element={<Blog />} />
+          <Route path="My-Website-Portfolio.github.io" element={<Home />} />
+          <Route path="My-Website-Portfolio.github.io/Home" element={<Home />}/>
+          <Route path="My-Website-Portfolio.github.io/Projects" element={<Projects />} />
+          <Route path="My-Website-Portfolio.github.io/resume" element={<Resume />}/>
+          <Route path="My-Website-Portfolio.github.io/blog" element={<Blog />} />
         </Routes>
         <footer className="App-footer">
           <Footer />
