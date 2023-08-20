0a4c0e2e6ec042f967af80cda25ca247a021d82d
TessaNyquist <tessanyquist@outlook.com>
2023-08-19 22:19:29 -0400

    Added Buttons

--- src/Components/SocialButton.js
+++ src/Components/SocialButton.js
@@ -1,25 +1,35 @@
 import React from "react";
 import "./SocialButtons.css";
+import ResumePdf from "../Images/Software-Developer-Résumé.pdf";
 
 function SocialButtons() {
+  const downloadResume = () => {
+    const element = document.createElement("a");
+    element.href = { ResumePdf }; // Replace with the actual path to your resume PDF
+    element.download = "Software-Developer-Resume.pdf";
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  };
   return (
     <div>
+      <a
+        className="DownloadButton"
+        href={ResumePdf}
+        onClick={downloadResume}
+      >Résumé</a>
       <a
         className="LinkedInButton"
         href="https://www.linkedin.com/in/tessa-nyquist/"
         target="_blank"
         rel="noopener noreferrer"
-      >
-        LinkedIn
-      </a>
+      >LinkedIn</a>
       <a
         className="GithubButton"
         href="https://github.com/"
         target="_blank"
         rel="noopener noreferrer"
-      >
-        GitHub
-      </a>
+      > GitHub</a>
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,43 +1,20 @@
 import AboutMe from "./Components/AboutMe";
-import ResumePdf from "./Images/Software-Developer-Résumé.pdf";
+//import ResumePdf from "./Images/Software-Developer-Résumé.pdf";
 import "./Home.css";
 import { Data } from "./Components/Data";
 import { ToolsData } from "./Components/ToolsData";
 import LanguageGrid from "./Components/LanguageGrid";
 import Accordion from "./Components/Accordion";
+import SocialButtons from "./Components/SocialButton";
 //import ContactMeForm from "./Components/ContactMe";
 
 function App() {
-  const downloadResume = () => {
-    const element = document.createElement("a");
-    element.href = ResumePdf;
-    element.download = "Software-Developer-Resume.pdf";
-    document.body.appendChild(element);
-    element.click();
-    document.body.removeChild(element);
-  };
   return (
     <div className="App">
       <AboutMe />
       <Accordion className="Accordian" />
       <div className="button-row">
-        <button className="home-button" onClick={downloadResume}>
-          Download Résumé
-        </button>
-        <a
-          href="https://www.linkedin.com/in/tessa-nyquist/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button className="home-button">LinkedIn</button>
-        </a>
-        <a
-          href="https://github.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button className="home-button">GitHub</button>
-        </a>
+        <SocialButtons />
       </div>
       <header className="App-header">
         <p className="skillsTitle"> Languages/Frameworks</p>
